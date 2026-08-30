3049050a6ba2ea05e53296233a6f85993cf31182
Rafat Khan <rafatkhan2210@gmail.com>
2026-08-30 12:04:19 +0600

    data_overview.sql

---
+++ sql_queries/data_overview.sql
@@ -0,0 +1,27 @@
+1. Data Overview
+
+SELECT
+  'Total Passengers' AS Column_Name,
+  COUNT(*)
+FROM airline_satisfaction  
+UNION ALL
+SELECT
+  'Satisfaction Rate',
+  ROUND(SUM(CASE
+    WHEN satisfaction ='Satisfied' THEN 1
+	ELSE 0
+  END)*100.0 / (SELECT COUNT(*) FROM airline_satisfaction),2)
+FROM airline_satisfaction
+UNION ALL
+SELECT
+  'Avg flight Distance(mi)',
+  ROUND(AVG(flight_distance),2)
+FROM airline_satisfaction
+UNION ALL
+SELECT
+ 'First-time Satisfaction %',
+    ROUND(SUM(CASE
+     WHEN customer_type='First-time' AND satisfaction ='Satisfied' THEN 1
+	 ELSE 0
+    END)*100.0/(SELECT SUM(CASE WHEN customer_type='First-time' THEN 1 ELSE 0 END) FROM airline_satisfaction),2)
+FROM airline_satisfaction
